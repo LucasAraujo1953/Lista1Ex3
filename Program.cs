913a769c988f673b4015b7d8bf63aa97bee87b8b
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-12 20:30:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            double a;
+
+            Console.Write("Informe a diagonal do quadrado: ");
+            d = double.Parse(Console.ReadLine());
+
+            a = Math.Pow(d, 2) / 2;
+            Console.WriteLine("O valor da diagonal do quadrado é {0}, com isso, sua área é igual a {1}", d, a);
+        }
+    }
+}
